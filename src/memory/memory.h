3b6257fa7f56d8739e1da9c6f1faa932ffc4fe2b
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-02 17:10:03 +0100

    feat(memory): NEW and ZEROS macros

--- src/memory/memory.h
+++ src/memory/memory.h
@@ -5,6 +5,9 @@
 #include "generic.h"
 #include "assertion.h"
 
+// -------------------------------------------------------------
+// | Memory Safety Allocator Macros|
+// -------------------------------------------------------------
 /**
  * XFREE(ptr):
  *      1. Frees the pointer if not NIL.
@@ -16,6 +19,28 @@
             ptr = NIL;                                              \
         } while (0)
 
+/**
+ * NEW(bytes):
+ *      1. Allocates `n` bytes from the heap.
+ *      2. Aborts the program if failed to allocate.
+ */
+#define NEW(n)                                                      \
+        new_( n )
+
+/**
+ * ZEROS(bytes):
+ *      1. Allocates `n` bytes from the heap and nullify them all.
+ *      2. Aborts the program if failed to allocate.
+ */
+#define ZEROS(n)                                                    \
+        zeros_( n )
+
+
+
+
+// -------------------------------------------------------------
+// | Memory Allocation Helpers |
+// -------------------------------------------------------------
 OWNED void * new_(COPIED u64 bytes);
 OWNED void * zeros_(COPIED u64 bytes);
 COPIED void * dispose(OWNED void * arg);
