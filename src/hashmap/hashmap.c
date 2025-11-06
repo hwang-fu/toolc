3e7c25a22682a63df532f14a3a5185499bd6ffee
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-06 19:33:47 +0100

    feat(hashmap): init

--- src/hashmap/hashmap.c
+++ src/hashmap/hashmap.c
@@ -1,2 +1,78 @@
 #include "hashmap.h"
 
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+struct HashmapEntry
+{
+    OWNED char * Key;
+    OWNED arch   Val;
+    OWNED HashmapEntry * Next;
+};
+
+OWNED Hashmap * hm_init(OWNED Hashmap * hm, u64 capacity, dispose_fn * cleanup)
+{
+}
+
+OWNED Hashmap * mk_hm(int mode, ...)
+{
+}
+
+void _hm_ins(BORROWED Hashmap * hm, BORROWED const char * key, arch val)
+{
+}
+
+void _hm_ins_owned_key(BORROWED Hashmap * hm, OWNED char * key, arch val)
+{
+}
+
+arch hm_get(BORROWED Hashmap * hm, BORROWED const char * key)
+{
+}
+
+arch hm_get_owned_key(BORROWED Hashmap * hm, OWNED char * key)
+{
+}
+
+void hm_del(BORROWED Hashmap * hm, BORROWED const char * key)
+{
+}
+
+void hm_del_owned_key(BORROWED Hashmap * hm, OWNED char * key)
+{
+}
+
+OWNED Result * hm_try_get(BORROWED Hashmap * hm, BORROWED const char * key)
+{
+}
+
+OWNED Result * hm_try_get_owned_key(BORROWED Hashmap * hm, OWNED char * key)
+{
+}
+
+OWNED Result * _hm_try_ins(BORROWED Hashmap * hm, BORROWED const char * key, arch val)
+{
+}
+
+OWNED Result * _hm_try_ins_owned_key(BORROWED Hashmap * hm, OWNED char * key, arch val)
+{
+}
+
+OWNED Result * hm_try_del(BORROWED Hashmap * hm, BORROWED const char * key)
+{
+}
+
+OWNED Result * hm_try_del_owned_key(BORROWED Hashmap * hm, OWNED char * key)
+{
+}
+
+u64 hm_get_size(BORROWED Hashmap * hm)
+{
+}
+
+OWNED Result * hm_try_get_size(BORROWED Hashmap * hm)
+{
+}
--- src/hashmap/hashmap.h
+++ src/hashmap/hashmap.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdarg.h>
+
 #include <hwangfu/generic.h>
 #include <hwangfu/result.h>
 #include <hwangfu/assertion.h>
@@ -8,21 +10,160 @@
 #define HASHMAP_DEFAULT_CAPACITY (20)
 #endif // HASHMAP_DEFAULT_CAPACITY
 
+#define hm_ins(hm, key, val)               _hm_ins(hm, key, CAST(val, arch))
+#define hm_ins_owned_key(hm, key, val)     _hm_ins_owned_key(hm, key, CAST(val, arch))
+#define hm_try_ins(hm, key, val)           _hm_try_ins(hm, key, CAST(val, arch))
+#define hm_try_ins_owned_key(hm, key, val) _hm_try_ins_owned_key(hm, key, CAST(val, arch))
+
 typedef struct Hashmap Hashmap;
 typedef struct HashmapEntry HashmapEntry;
 
-struct HashmapEntry
-{
-    owned char * Key;
-    owned arch   Val;
-    owned HashmapEntry * Next;
-};
-
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
 struct Hashmap
 {
-    copied   u64             Capacity   ;
-    copied   u64             Size       ;
-    owned    HashmapEntry ** Buckets    ;
-    borrowed dispose_fn    * Dispose    ;
+    COPIED   u64             Capacity   ;
+    COPIED   u64             Size       ;
+    OWNED    HashmapEntry ** Buckets    ;
+    BORROWED dispose_fn    * Dispose    ;
 };
 
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Hashmap * hm_init(OWNED Hashmap * hm, u64 capacity, dispose_fn * cleanup);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Hashmap * mk_hm(int mode, ...);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+void _hm_ins(BORROWED Hashmap * hm, BORROWED const char * key, arch val);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+void _hm_ins_owned_key(BORROWED Hashmap * hm, OWNED char * key, arch val);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+arch hm_get(BORROWED Hashmap * hm, BORROWED const char * key);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+arch hm_get_owned_key(BORROWED Hashmap * hm, OWNED char * key);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+void hm_del(BORROWED Hashmap * hm, BORROWED const char * key);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+void hm_del_owned_key(BORROWED Hashmap * hm, OWNED char * key);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Result * hm_try_get(BORROWED Hashmap * hm, BORROWED const char * key);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Result * hm_try_get_owned_key(BORROWED Hashmap * hm, OWNED char * key);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Result * _hm_try_ins(BORROWED Hashmap * hm, BORROWED const char * key, arch val);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Result * _hm_try_ins_owned_key(BORROWED Hashmap * hm, OWNED char * key, arch val);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Result * hm_try_del(BORROWED Hashmap * hm, BORROWED const char * key);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Result * hm_try_del_owned_key(BORROWED Hashmap * hm, OWNED char * key);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+u64 hm_get_size(BORROWED Hashmap * hm);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+OWNED Result * hm_try_get_size(BORROWED Hashmap * hm);
+
+/**
+ * @since       06.11.2025
+ * @author      Junzhe
+ * @modified    06.11.2025
+ *
+ */
+COPIED void * hm_dispose(OWNED void * arg);
