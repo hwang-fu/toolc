885ca2d3606ce34bbefe290e36ea069320d0b868
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-06 11:49:43 +0100

    test(dequeue): test case update

--- test/dequeue/test.c
+++ test/dequeue/test.c
@@ -18,6 +18,28 @@
         dq_pushback(dq, 3);
         dq_pushback(dq, 4);
         dq_pushback(dq, 5);
+
+        ASSERT_EXPR(EQ(
+                    dq_popback(dq),
+                    5
+        ));
+        ASSERT_EXPR(EQ(
+                    dq_popback(dq),
+                    4
+        ));
+        ASSERT_EXPR(EQ(
+                    dq_popback(dq),
+                    3
+        ));
+        ASSERT_EXPR(EQ(
+                    dq_popback(dq),
+                    2
+        ));
+        ASSERT_EXPR(EQ(
+                    dq_popback(dq),
+                    1
+        ));
+
         dq_dispose(dq);
         pass(cases++);
     }
