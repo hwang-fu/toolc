1d3eb0a9fd717afdf57a2ba82450cbdc4c24c782
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-07 18:16:06 +0100

    feat(pair): make pair

--- src/pair/pair.c
+++ src/pair/pair.c
@@ -24,6 +24,54 @@ OWNED Pair * pair_init(OWNED Pair * pair, arch fst, arch snd, dispose_fn * clean
  */
 OWNED Pair * mk_pair(int mode, ...)
 {
+    va_list ap;
+    va_start(ap, mode);
+
+    arch fst = 0;
+    arch snd = 0;
+
+    dispose_fn * cleanup1 = NIL;
+    dispose_fn * cleanup2 = NIL;
+
+    switch (mode)
+    {
+        case 0:
+        {
+            fst = va_arg(ap, arch);
+            snd = va_arg(ap, arch);
+        } break;
+
+        case 1:
+        {
+            fst = va_arg(ap, arch);
+            snd = va_arg(ap, arch);
+            cleanup1 = va_start(ap, dispose_fn*);
+            cleanup2 = va_start(ap, dispose_fn*);
+        } break;
+
+        case 2:
+        {
+            fst = va_arg(ap, arch);
+            snd = va_arg(ap, arch);
+            cleanup1 = va_start(ap, dispose_fn*);
+        } break;
+
+        case 3:
+        {
+            fst = va_arg(ap, arch);
+            snd = va_arg(ap, arch);
+            cleanup2 = va_start(ap, dispose_fn*);
+        } break;
+
+        default:
+        {
+            PANIC("%s(): unkown mode %d", mode);
+        } break;
+    }
+
+    va_end(ap);
+
+    return pair_init(NIL, fst, snd, cleanup1, cleanup2);
 }
 
 arch pair_fst(BORROWED Pair * pair)
@@ -31,6 +79,14 @@ arch pair_fst(BORROWED Pair * pair)
 }
 
 arch pair_snd(BORROWED Pair * pair)
+{
+}
+
+OWNED Result * pair_try_fst(BORROWED Pair * pair)
+{
+}
+
+OWNED Result * pair_try_snd(BORROWED Pair * pair)
 {
 }
 
@@ -47,6 +103,7 @@ COPIED void * pair_dispose(OWNED void * arg)
     {
         pair->DisposeFirst(CAST(pair->First, void*));
     }
+
     if (pair->DisposeSecond)
     {
         pair->DisposeSecond(CAST(pair->Second, void*));
