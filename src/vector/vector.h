a0c39615b2fa47af2582d00c9611e9f77e56597c
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-16 22:09:29 +0100

    docs(vector): get size, get capacity, dispose [skip ci]

--- src/vector/vector.h
+++ src/vector/vector.h
@@ -43,3 +43,48 @@ OWNED Vector * vector_init(OWNED Vector * vec, u64 capacity);
  * @li OWNED Vector * mk_vector(1, u64 capacity)
  */
 OWNED Vector * mk_vector(int mode, ...);
+
+
+_vector_pushfront();
+
+_vector_pushback();
+
+_vector_try_pushfront();
+
+_vector_try_pushback();
+
+
+/**
+ * @since       16.11.2025
+ * @author      Junzhe
+ * @modified    16.11.2025
+ */
+u64 vector_get_size(BORROWED Vector * vec);
+
+/**
+ * @since       16.11.2025
+ * @author      Junzhe
+ * @modified    16.11.2025
+ */
+OWNED Result * vector_try_get_size(BORROWED Vector * vec);
+
+/**
+ * @since       16.11.2025
+ * @author      Junzhe
+ * @modified    16.11.2025
+ */
+u64 vector_get_capacity(BORROWED Vector * vec);
+
+/**
+ * @since       16.11.2025
+ * @author      Junzhe
+ * @modified    16.11.2025
+ */
+OWNED Result * vector_try_get_capacity(BORROWED Vector * vec);
+
+/**
+ * @since       16.11.2025
+ * @author      Junzhe
+ * @modified    16.11.2025
+ */
+COPIED void * vector_dispose(OWNED void * arg);
